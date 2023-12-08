a03e85b513e88cedeae3be3d9a4d6c2b76dde92d
Obed Effum <obedeffum10@gmail.com>
2023-12-08 19:31:13 +0000

    edit update

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,32 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-
 const LandingPage = () => {
-     //Initial array of objects to be obtained from the form
-     const [storedData, setStoredData] = useState([]);
-     const [formData, setFormData] = useState(
-       {
-         ussdCode: "",
-         ussdUrl: "",
-         phoneNumber: "",
-         networkName: "",
-       },
-     );
+  //Initial array of objects to be obtained from the form
+  const [storedData, setStoredData] = useState([]);
+  const [formData, setFormData] = useState({
+    ussdCode: "",
+    ussdUrl: "",
+    phoneNumber: "",
+    networkName: "",
+  });
 
-
-      //Adds the USSD code to the array after the submit button has been clicked
+  //Adds the USSD code to the array after the submit button has been clicked
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const newData = { ...formData, id: Date.now() }; // Add an "id" property
     setStoredData([...storedData, newData]);
-    
 
     //stores the formData to the local browser storage
-    localStorage.setItem("ussdCodes", JSON.stringify([...storedData,newData]));
+    localStorage.setItem("ussdCodes", JSON.stringify([...storedData, newData]));
 
     setFormData({
-
       ussdCode: "",
       ussdUrl: "",
       phoneNumber: "",
@@ -35,9 +29,8 @@ const LandingPage = () => {
   };
 
   //State for update mode
-  const [updateMode, setUpdateMode] = useState(false)
+  const [updateMode, setUpdateMode] = useState(false);
 
-  
   useEffect(() => {
     // Retrieve data from localStorage
     const storedFormData = localStorage.getItem("ussdCodes");
@@ -55,23 +48,89 @@ const LandingPage = () => {
     }));
   };
 
- 
-
+  //Deleting data from the local Storage
   const handleDelete = (itemToDelete) => {
-    const updatedData = storedData.filter(item => item.id !== itemToDelete.id);
-
-    localStorage.removeItem("ussdCodes", JSON.stringify(updatedData));
-    setStoredData(updatedData);
+    const newData = storedData.filter((item) => item.id !== itemToDelete);
+    setStoredData(newData);
+    // localStorage.removeItem(newData);
+    localStorage.setItem("ussdCodes", JSON.stringify(newData));
+    // setStoredData();
+    console.log(storedData);
+    console.log(newData);
   };
 
-//update mode functionality
-const toggleUpdateMode = (passedID) => {
-  setUpdateMode(true);
- 
-  JSON.parse(localStorage.getItem("ussdCodes"));
+  //update mode functionality
+  const toggleUpdateMode = (passedID) => {
+    // console.log(passedID)
+    setUpdateMode(true);
 
 
-};
+    const codes = JSON.parse(localStorage.getItem("ussdCodes"));
+    const data = codes.find((obj) => obj.id === passedID);
+
+    setFormData({
+      passedID,
+      ussdCode: data.ussdCode,
+      ussdUrl: data.ussdUrl,
+      phoneNumber: data.phoneNumber,
+      networkName: data.networkName,
+    });
+  };
+
+  // const handleUpdate = (updatedFormData) => {
+  //   // Find the index of the item to be updated
+  //   const indexToUpdate = storedData.findIndex((item) => item.id === updatedFormData.id);
+  
+  //   // If the item is found, update its properties
+  //   if (indexToUpdate !== -1) {
+  //     const newData = { ...formData, id: updatedFormData.id }; // Add an "id" property
+  //   setStoredData([...storedData]);
+
+  //   //stores the formData to the local browser storage
+  //   localStorage.setItem("ussdCodes", JSON.stringify([...storedData]));
+       
+  //     };
+
+      
+  
+      // Update the state and localStorage with the modified data
+      // setStoredData(updatedData1);
+      // localStorage.setItem("ussdCodes", JSON.stringify(updatedData1));
+  
+      // Reset the form data and update mode
+    //   setFormData({
+    //     ussdCode: "",
+    //     ussdUrl: "",
+    //     phoneNumber: "",
+    //     networkName: "",
+    //   });
+    //   setUpdateMode(false);
+    // }
+  
+    const handleUpdate = () => {
+      
+      const filteredData = storedData.filter(data => data.id !== formData.passedID); 
+    setStoredData([...filteredData,formData]);
+
+    //stores the formData to the local browser storage
+    localStorage.setItem("ussdCodes", JSON.stringify([...filteredData, formData]));
+
+    setFormData({
+      ussdCode: "",
+      ussdUrl: "",
+      phoneNumber: "",
+      networkName: "",
+    });
+
+
+
+      
+    }
+
+
+
+
+
 
 
 
@@ -83,111 +142,111 @@ const toggleUpdateMode = (passedID) => {
           <div className="p-3 font-bold text-xl">Add new USSD</div>
           <div className="w-[400px] h-[430px] border-2 flex flex-col">
             {!updateMode ? (
-                  <form onSubmit={handleSubmit} class="p-2 flex flex-col ">
-                  <div className="p-1">
-                    <div className="">Enter USSD code:</div>
-                    <input
-                      name="ussdCode"
-                      value={formData.ussdCode}
-                      onChange={handleChange}
-                      class="required: w-[260px]  border-2 rounded border-black py-1 px-1"
-                    />
-                  </div>
-                  <div className="p-1">
-                    <div className="">Enter USSD URL:</div>
-                    <input
-                      name="ussdUrl"
-                      value={formData.ussdUrl}
-                      onChange={handleChange}
-                      class="required: w-[260px] border-2 rounded border-black py-1 px-1"
-                    />
-                  </div>
-                  <div className="p-1">
-                    <div className="">Enter Phone Number:</div>
-                    <input
-                      name="phoneNumber"
-                      value={formData.phoneNumber}
-                      onChange={handleChange}
-                      class="required: w-[260px] border-2 rounded border-black py-1 px-1"
-                    />
-                  </div>
-                  <div className="p-1">
-                    <div className="">Enter Network Name:</div>
-                    <input
-                      name="networkName"
-                      value={formData.networkName}
-                      onChange={handleChange}
-                      class="required: w-[260px] border-2 rounded border-black py-1 px-1"
-                    />
-                  </div>
-                  <div className=" w-full items-center text-center p-3">
-                    <button
-                      class="items-center border-solid bg-blue-400 text-white h-[50px] w-[80px] rounded hover:bg-blue-800"
-                      type="submit"
-                    >
-                      Add USSD
-                    </button>
-                  </div>
-                  {console.log(storedData)}
-                </form>
-                  
-            ): (
-              <form  class="p-2 flex flex-col ">
-              <div className="p-1">
-                <div className="">Enter USSD code:</div>
-                <input
-                  name="ussdCode"
-                  
-                  onChange={handleChange}
-                  class="required: w-[260px]  border-2 rounded border-black py-1 px-1"
-                />
-              </div>
-              <div className="p-1">
-                <div className="">Enter USSD URL:</div>
-                <input
-                  name="ussdUrl"
-                  
-                  onChange={handleChange}
-                  class="required: w-[260px] border-2 rounded border-black py-1 px-1"
-                />
-              </div>
-              <div className="p-1">
-                <div className="">Enter Phone Number:</div>
-                <input
-                  name="phoneNumber"
-                 
-                  onChange={handleChange}
-                  class="required: w-[260px] border-2 rounded border-black py-1 px-1"
-                />
-              </div>
-              <div className="p-1">
-                <div className="">Enter Network Name:</div>
-                <input
-                  name="networkName"
-                 
-                  onChange={handleChange}
-                  class="required: w-[260px] border-2 rounded border-black py-1 px-1"
-                />
-              </div>
-              <div className=" flex justify-between w-full items-center text-center p-3">
-                <button
-                  class="items-center border-solid bg-blue-400 text-white h-[50px] w-[80px] rounded hover:bg-blue-800"
-                  type="submit"
-                >
-                  Update
-                </button>
-                <button
-                  onClick={toggleUpdateMode}
-                  class="items-center border-solid bg-blue-400 text-white h-[50px] w-[80px] rounded hover:bg-blue-800"
-                  type="submit"
-                >
-                  Cancel
-                </button>
-              </div>
-              {console.log(storedData)}
-            </form>
+              <form onSubmit={handleSubmit} className="p-2 flex flex-col ">
+                <div className="p-1">
+                  <div className="">Enter USSD code:</div>
+                  <input
+                    name="ussdCode"
+                    value={formData.ussdCode}
+                    onChange={handleChange}
+                    className="required: w-[260px]  border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className="p-1">
+                  <div className="">Enter USSD URL:</div>
+                  <input
+                    name="ussdUrl"
+                    value={formData.ussdUrl}
+                    onChange={handleChange}
+                    className="required: w-[260px] border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className="p-1">
+                  <div className="">Enter Phone Number:</div>
+                  <input
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    className="required: w-[260px] border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className="p-1">
+                  <div className="">Enter Network Name:</div>
+                  <input
+                    name="networkName"
+                    value={formData.networkName}
+                    onChange={handleChange}
+                    className="required: w-[260px] border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className=" w-full items-center text-center p-3">
+                  <button
+                    className="items-center border-solid bg-blue-400 text-white h-[50px] w-[80px] rounded hover:bg-blue-800"
+                    type="submit"
+                  >
+                    Add USSD
+                  </button>
+                </div>
+                {/* {console.log(storedData)} */}
+              </form>
+            ) : (
+              <form  onSubmit={ handleUpdate} className="p-2 flex flex-col ">
+                <div className="p-1">
+                  <div className="">Enter USSD code:</div>
+                  <input
+                    
+                    name="ussdCode"
+                    value={formData.ussdCode}
+                    onChange={handleChange}
+                    className="required: w-[260px]  border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className="p-1">
+                  <div className="">Enter USSD URL:</div>
+                  <input
+                    name="ussdUrl"
+                    value={formData.ussdUrl}
+                    onChange={handleChange}
+                    className="required: w-[260px] border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className="p-1">
+                  <div className="">Enter Phone Number:</div>
+                  <input
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    className="required: w-[260px] border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className="p-1">
+                  <div className="">Enter Network Name:</div>
+                  <input
+                    name="networkName"
+                    value={formData.networkName}
+                    onChange={handleChange}
+                    className="required: w-[260px] border-2 rounded border-black py-1 px-1"
+                  />
+                </div>
+                <div className=" flex justify-between w-full items-center text-center p-3">
+                  <button
+                    
+                    className="items-center border-solid bg-blue-400 text-white h-[50px] w-[80px] rounded hover:bg-blue-800"
+                    type="submit"
+                  >
+                    Update
+                  </button>
+                  <button
+                   
+                    className="items-center border-solid bg-blue-400 text-white h-[50px] w-[80px] rounded hover:bg-blue-800"
+                    onClick={() => setUpdateMode(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+               
+              </form>
             )}
-            
           </div>
         </div>
         <div className="w-[500px] h-[500px] border-[3px] flex flex-col items-center">
@@ -195,7 +254,6 @@ const toggleUpdateMode = (passedID) => {
           <div className="p-2 w-full grid grid-cols-2 gap-y-2 gap-x-2">
             {storedData && storedData.length > 0 ? (
               storedData.map((data, index) => (
-                  
                 <div
                   key={index}
                   className=" bg-slate-500 flex flex-col w-auto p-2 rounded text-white "
@@ -209,7 +267,10 @@ const toggleUpdateMode = (passedID) => {
                     </ul>
                   </div>
                   <div className="w-auto flex justify-between p-1">
-                    <button onClick={()=>toggleUpdateMode(data.id)} className="items-center border-solid bg-blue-400 text-white h-[30px] w-[40px] rounded hover:bg-blue-800">
+                    <button
+                      onClick={() => toggleUpdateMode(data.id)}
+                      className="items-center border-solid bg-blue-400 text-white h-[30px] w-[40px] rounded hover:bg-blue-800"
+                    >
                       Edit
                     </button>
                     <button className="items-center border-solid bg-blue-400 text-white h-[30px] w-[40px] rounded hover:bg-blue-800">
@@ -217,7 +278,7 @@ const toggleUpdateMode = (passedID) => {
                     </button>
                     <button
                       className="items-center border-solid bg-blue-400 text-white h-[30px] w-[60px] rounded hover:bg-blue-800"
-                      onClick={()=> handleDelete(data)}
+                      onClick={() => handleDelete(data.id)}
                     >
                       Delete
                     </button>
@@ -225,7 +286,9 @@ const toggleUpdateMode = (passedID) => {
                 </div>
               ))
             ) : (
-              <div className="w-auto flex text-center items-center"><p >No codes stored yet</p></div>
+              <div className="w-auto flex text-center items-center">
+                <p>No codes stored yet</p>
+              </div>
             )}
           </div>
         </div>
